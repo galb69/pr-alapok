5433e887c973efd493b12995f8154c5e24089acc
galb69 <128583360+galb69@users.noreply.github.com>
2023-10-12 09:28:25 +0200

    Add files via upload

---
+++ szamonkeres_001.py
@@ -0,0 +1,23 @@
+#Gál Bence 10.D
+#szamonkeres_001
+"""
+be kérek egy osztályzatot 1-5 irasssuk ki a megadott jegyet értékkel és szövegesen,
+ha nem megfelelő jegyet  adtam akkor írjuk ki a jegy értékét és mellé azt a szöveget hogy nem megfelelő
+"""
+ertek = input ("kerek egy osztalyzatot.")
+ertek = int(ertek)
+
+if ertek == 5 :
+ print ("5,jeles")
+
+elif ertek == 4 :
+print = ("4, jo")
+
+elif ertek == 3 :
+print = ("3, kozepes") 
+
+elif ertek == 2 :
+print = ("2, elegseges") 
+
+elif ertek == 1 :
+print = ("1, elegtelen") 
